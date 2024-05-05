152b7d7e5320b033071b1318b8d41075dd9b0903
nkordis <nikoskordis@gmail.com>
2024-05-06 00:41:05 +0200

    feat: add Tag model

--- app/core/models.py
+++ app/core/models.py
@@ -53,6 +53,19 @@ class Project(models.Model):
     )
     title = models.CharField(max_length=255)
     bodyText = models.TextField(blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    """Tag for filtering projects."""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
