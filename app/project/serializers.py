38874107adcad0ccb6c6544a443688d375d08ae4
nkordis <nikoskordis@gmail.com>
2024-05-08 00:30:18 +0200

    feat: implement create tag feature

--- app/project/serializers.py
+++ app/project/serializers.py
@@ -6,21 +6,6 @@ from rest_framework import serializers
 from core.models import Project, Tag
 
 
-class ProjectSerializer(serializers.ModelSerializer):
-    """Serializer for the projects."""
-
-    class Meta:
-        model = Project
-        fields = ("id", 'title', 'bodyText')
-        read_only_fields = ('id',)
-
-
-class ProjectDetailSerializer(ProjectSerializer):
-    """Serializer for the project detail."""
-    class Meta(ProjectSerializer.Meta):
-        fields = ProjectSerializer.Meta.fields + ()
-
-
 class TagSerializer(serializers.ModelSerializer):
     """Serializer for the tags."""
 
@@ -28,3 +13,34 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = ('id', 'name')
         read_only_fields = ('id',)
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    """Serializer for the projects."""
+    tags = TagSerializer(many=True, required=False)
+
+    class Meta:
+        model = Project
+        fields = ("id", 'title', 'bodyText', 'tags')
+        read_only_fields = ('id',)
+
+    def create(self, validated_data):
+        """Create a new project."""
+        tags = validated_data.pop('tags', [])
+        project = Project.objects.create(**validated_data)
+
+        auth_user = self.context['request'].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(
+                user=auth_user,
+                **tag
+            )
+            project.tags.add(tag_obj)
+
+        return project
+
+
+class ProjectDetailSerializer(ProjectSerializer):
+    """Serializer for the project detail."""
+    class Meta(ProjectSerializer.Meta):
+        fields = ProjectSerializer.Meta.fields + ()
