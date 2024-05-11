3447d8436df95eb6f7984412211e59762a0c653e
nkordis <nikoskordis@gmail.com>
2024-05-11 10:01:49 +0200

    test: add tests for listing links

---
+++ app/project/tests/test_links_api.py
@@ -0,0 +1,76 @@
+"""
+Tests for the links API.
+"""
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Link
+
+from project.serializers import LinkSerializer
+
+
+LINKS_URL = reverse('project:link-list')
+
+
+def create_user(email='user@example.com', password='testpass123'):
+    """Create and return a user."""
+    return get_user_model().objects.create_user(email=email, password=password)
+
+
+class PublicLinksApiTests(TestCase):
+    """Test unauthenticated API requests."""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        """Test auth is required to call API."""
+        res = self.client.get(LINKS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateLinksApiTests(TestCase):
+    """Test authenticated API requests."""
+    def setUp(self):
+        self.user = create_user()
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_links(self):
+        """Test retrieving a list of links."""
+        Link.objects.create(user=self.user,
+                            text='Test link 1',
+                            href='http://example.com')
+        Link.objects.create(user=self.user,
+                            text='Test link 2',
+                            href='http://example.com')
+
+        res = self.client.get(LINKS_URL)
+
+        links = Link.objects.all().order_by('-text')
+        serializer = LinkSerializer(links, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_links_limited_to_user(self):
+        """Test list of links is limited to authenticated user."""
+        user2 = create_user(email='user2@example.com')
+        Link.objects.create(user=user2,
+                            text='Test link 1',
+                            href='http://example.com')
+        link = Link.objects.create(user=self.user,
+                                   text='Test link 2',
+                                   href='http://example.com')
+
+        res = self.client.get(LINKS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['text'], link.text)
+        self.assertEqual(res.data[0]['href'], link.href)
+        self.assertEqual(res.data[0]['id'], link.id)
