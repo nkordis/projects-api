949dc54fb24256db0924a1205606ffd8f8ba63af
nkordis <nikoskordis@gmail.com>
2024-05-24 16:28:43 +0200

    chore: add Terraform deploy configuration

---
+++ infra/deploy/variables.tf
@@ -0,0 +1,14 @@
+variable "prefix" {
+  description = "Prefix for resources"
+  default     = "pa"
+}
+
+variable "project" {
+  description = "Name of project"
+  default     = "projects-api"
+}
+
+variable "contact" {
+  description = "Contact email address"
+  default     = "nikoskordis@gmail.com"
+}
