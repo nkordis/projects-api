f44ae9d7dffa69bd3b36874d8188f77b920a42c1
nkordis <nikoskordis@gmail.com>
2024-05-29 11:26:10 +0200

    fix: correct vpc id reference in aws_security_group

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -114,21 +114,21 @@ resource "aws_subnet" "private_b" {
 
 resource "aws_security_group" "endpoint_access" {
   description = "Access to endpoints"
-  name = "${local.prefix}-endpoint-access"
-  vpc_id = aws_vpc_main.id
+  name        = "${local.prefix}-endpoint-access"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     cidr_blocks = [aws_vpc.main.cidr_block]
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
   }
 }
 
 resource "aws_vpc_endpoint" "ecr" {
-  vpc_id = aws_vpc.main.id
-  service_name = "com.amazonaws.${data.aws_region.current.name}.ecr.api"
-  vpc_endpoint_type = "Interface"
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ecr.api"
+  vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
 
   subnet_ids = [aws_subnet.private_a.id, aws_subnet.private_b.id]
@@ -143,9 +143,9 @@ resource "aws_vpc_endpoint" "ecr" {
 }
 
 resource "aws_vpc_endpoint" "dkr" {
-  vpc_id = aws_vpc.main.id
-  service_name = "com.amazonaws.${data.aws_region.current.name}.ecr.dkr"
-  vpc_endpoint_type = "Interface"
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ecr.dkr"
+  vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
 
   subnet_ids = [aws_subnet.private_a.id, aws_subnet.private_b.id]
@@ -160,9 +160,9 @@ resource "aws_vpc_endpoint" "dkr" {
 }
 
 resource "aws_vpc_endpoint" "cloudwatch_logs" {
-  vpc_id = aws_vpc.main.id
-  service_name = "com.amazonaws.${data.aws_region.current.name}.logs"
-  vpc_endpoint_type = "Interface"
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.logs"
+  vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
 
   subnet_ids = [aws_subnet.private_a.id, aws_subnet.private_b.id]
@@ -177,9 +177,9 @@ resource "aws_vpc_endpoint" "cloudwatch_logs" {
 }
 
 resource "aws_vpc_endpoint" "ssm" {
-  vpc_id = aws_vpc.main.id
-  service_name = "com.amazonaws.${data.aws_region.current.name}.ssmmessages"
-  vpc_endpoint_type = "Interface"
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ssmmessages"
+  vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
 
   subnet_ids = [aws_subnet.private_a.id, aws_subnet.private_b.id]
@@ -194,8 +194,8 @@ resource "aws_vpc_endpoint" "ssm" {
 }
 
 resource "aws_vpc_endpoint" "s3" {
-  vpc_id = aws_vpc.main.id
-  service_name = "com.amazonaws.${data.aws_region.current.name}.s3"
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${data.aws_region.current.name}.s3"
   vpc_endpoint_type = "Gateway"
   route_table_ids = [
     aws_vpc.main.default_route_table_id
